0a5b0bdaf52cc3550aebd4f0e7c61d80d04376de
Youjung Heo <hujumee27@gmail.com>
2021-12-24 23:41:18 +0900

    7th seminar level1 assignment

---
+++ 7th-seminar/functions/api/routes/post/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const { checkUser } = require('../../../middlewares/auth');
+const uploadImage = require('../../../middlewares/uploadImage');
+
+router.get('/list', require('./postListGET'));
+router.post('/', uploadImage, require('./postPOST'));
+
+router.get('/:postId', checkUser, require('./postGET'));
+router.put('/:postId', require('./postPUT'));
+router.delete('/:postId', require('./postDELETE'));
+
+module.exports = router;
